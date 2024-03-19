4723c4720306a88204c4541f3e4835bc34abf309
Kimkideok-Duke <113305918+vpazhroxoq@users.noreply.github.com>
2024-03-19 09:54:34 +0900

    깃생성

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Movie from './pages/Movie';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route>
+        {/* 영화차트 */}
+        <Route path='/' element={<Movie/>} />
+      </Route>
+    </BrowserRouter>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="/Movie.js"
+    //       target="/Movie.js"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/pages/Movie.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Movie() {
+  return (
+    <div className="Movie">
+      <header className="Movie-header">
+        <p>
+          영화 목록
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Movie;
